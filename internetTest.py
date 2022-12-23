49473c5fa726709ca82cb3e11910ff63419d139a
Owen DePoint <56412242+odepoint@users.noreply.github.com>
2022-12-22 20:41:32 -0500

    Update internetTest.py

--- internetTest.py
+++ internetTest.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def check_internet():
   try:
-    # create a socket and send a request to 8.8.8.8 (Google's Primary DNS server)
+    # creates a socket and sends a request to 8.8.8.8 (Google's Primary DNS server)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("8.8.8.8", 53))
     s.close()
@@ -20,17 +20,17 @@ try:
   pd.read_csv('internet_status.csv') 
 except pd.errors.EmptyDataError:
   with open('internet_status.csv', 'w', newline='') as csvfile:
-    #  initialize the CSV file
+    #  initializes the CSV file
     writer = csv.writer(csvfile)
     writer.writerow(['Timestamp', 'Is offline'])
 
 count = 1;
 while True:
-  # check if the internet is offline
+  # checks if the internet is offline
   is_offline = not check_internet()
 
 
-  # get the current timestamp in the local system time
+  # gets the current timestamp in the local system time
   timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
   
 
@@ -39,11 +39,11 @@ while True:
     if(is_offline):
       count+=1
     if(count > 0): 
-      # update the CSV file with the current status
+      # updates the CSV file with the current status
       writer.writerow([timestamp, is_offline])
       count+=1
     if(count > 3): # stops displaying succesful requests
       count = 0
   
-  # wait for 30 seconds before checking again
+  # waits for 30 seconds before checking again
   time.sleep(30)
